beaff687195cdc63265f475faaaafb05fac2d049
Sahishnu Pawan Kumar <sahishnu.pawankumar@research.iiit.ac.in>
2026-04-12 19:22:21 +0530

    question 2

--- q2/q2.s
+++ q2/q2.s
@@ -1,132 +1,141 @@
-.cstring
+.section .rodata
 format_str: .asciz "%d "
 newline_str: .asciz "\n"
 
 .text
-.globl _main
+.globl main
 
-_main:
-	pushq %rbp
-	movq %rsp, %rbp
-	subq $32, %rsp
+main:
+	addi sp, sp, -80
+	sd ra, 72(sp)
+	sd s0, 64(sp)
+	sd s1, 56(sp)
+	sd s2, 48(sp)
+	sd s3, 40(sp)
+	sd s4, 32(sp)
+	sd s5, 24(sp)
+	sd s6, 16(sp)
+	sd s7, 8(sp)
+	sd s8, 0(sp)
 
-	cmpl $2, %edi
-	jl L_end
+	addi s0, a0, 0
+	addi s1, a1, 0
 
-	movq %rsi, -8(%rbp)
-	decl %edi
-	movl %edi, -20(%rbp)
+	addi t0, x0, 2
+	blt s0, t0, L_end
 
-	movslq %edi, %rdi
-	imulq $12, %rdi
-	call _malloc
-	movq %rax, -16(%rbp)
+	addi s2, s0, -1
 
-	movl $0, -24(%rbp)
+	slli t0, s2, 3
+	slli t1, s2, 2
+	add a0, t0, t1
+	jal ra, malloc
+	addi s3, a0, 0
+
+	addi s4, x0, 0
 L_parse_loop:
-	movl -24(%rbp), %eax
-	cmpl -20(%rbp), %eax
-	jge L_parse_done
+	bge s4, s2, L_parse_done
 
-	movq -8(%rbp), %rsi
-	movl %eax, %ecx
-	incl %ecx
-	movslq %ecx, %rcx
-	movq (%rsi, %rcx, 8), %rdi
-	
-	call _atoi
+	addi t0, s4, 1
+	slli t0, t0, 3
+	add t0, s1, t0
+	ld a0, 0(t0)
+	jal ra, atoi
 
-	movl -24(%rbp), %ecx
-	movslq %ecx, %rcx
-	movq -16(%rbp), %r8
+	slli t1, s4, 2
+	add t2, s3, t1
+	sw a0, 0(t2)
 
-	movl %eax, (%r8, %rcx, 4)
+	slli t3, s2, 2
+	add t4, s3, t3
+	add t4, t4, t1
+	addi t5, x0, -1
+	sw t5, 0(t4)
 
-	movl -20(%rbp), %edx
-	movslq %edx, %rdx
-	leaq (%r8, %rdx, 4), %r9
-	
-	movl $-1, (%r9, %rcx, 4)
-
-	incl -24(%rbp)
-	jmp L_parse_loop
+	addi s4, s4, 1
+	jal x0, L_parse_loop
 L_parse_done:
 
-	movq -16(%rbp), %r8
-	movl -20(%rbp), %r11d
-	movslq %r11d, %rdx
+	slli t0, s2, 2
+	add s5, s3, t0
+	add s6, s5, t0
 
-	leaq (%r8, %rdx, 4), %r9
-	leaq (%r9, %rdx, 4), %r10
-
-	xorl %eax, %eax
-	xorl %ecx, %ecx
+	addi s7, x0, 0
+	addi s4, x0, 0
 
 L_nge_loop:
-	cmpl %r11d, %ecx
-	jge L_nge_done
+	bge s4, s2, L_nge_done
 
-	movslq %ecx, %rdi
-	movl (%r8, %rdi, 4), %edx
+	slli t1, s4, 2
+	add t2, s3, t1
+	lw s8, 0(t2)
 
 L_nge_inner:
-	cmpl $0, %eax
-	je L_nge_push
+	beq s7, x0, L_nge_push
 
-	movl %eax, %esi
-	decl %esi
-	movslq %esi, %rsi
-	movl (%r10, %rsi, 4), %edi
+	addi t0, s7, -1
+	slli t0, t0, 2
+	add t0, s6, t0
+	lw t1, 0(t0)
 
-	movslq %edi, %rdi
-	movl (%r8, %rdi, 4), %esi
+	slli t2, t1, 2
+	add t2, s3, t2
+	lw t3, 0(t2)
 
-	cmpl %edx, %esi
-	jge L_nge_push
+	bge t3, s8, L_nge_push
 
-	movl %ecx, (%r9, %rdi, 4)
-	
-	decl %eax
-	jmp L_nge_inner
+	slli t2, t1, 2
+	add t2, s5, t2
+	sw s4, 0(t2)
+
+	addi s7, s7, -1
+	jal x0, L_nge_inner
 
 L_nge_push:
-	movslq %eax, %rsi
-	movl %ecx, (%r10, %rsi, 4) 
-	incl %eax
-	
-	incl %ecx
-	jmp L_nge_loop
+	slli t0, s7, 2
+	add t0, s6, t0
+	sw s4, 0(t0)
+
+	addi s7, s7, 1
+	addi s4, s4, 1
+	jal x0, L_nge_loop
 L_nge_done:
 
-	movl $0, -24(%rbp)
+	addi s4, x0, 0
 L_print_loop:
-	movl -24(%rbp), %eax
-	cmpl -20(%rbp), %eax
-	jge L_print_done
+	bge s4, s2, L_print_done
 
-	movslq %eax, %rcx
-	movq -16(%rbp), %r8
-	movl -20(%rbp), %edx
-	movslq %edx, %rdx
-	leaq (%r8, %rdx, 4), %r9
+	slli t0, s4, 2
+	add t0, s5, t0
+	lw a1, 0(t0)
 
-	movl (%r9, %rcx, 4), %esi
-	leaq format_str(%rip), %rdi
-	xorl %eax, %eax
-	call _printf
+	lui a0, %hi(format_str)
+	addi a0, a0, %lo(format_str)
+	jal ra, printf
 
-	incl -24(%rbp)
-	jmp L_print_loop
+	addi s4, s4, 1
+	jal x0, L_print_loop
 L_print_done:
 
-	leaq newline_str(%rip), %rdi
-	xorl %eax, %eax
-	call _printf
+	lui a0, %hi(newline_str)
+	addi a0, a0, %lo(newline_str)
+	jal ra, printf
 
-	movq -16(%rbp), %rdi
-	call _free
+	addi a0, s3, 0
+	jal ra, free
 
 L_end:
-	xorl %eax, %eax
-	leave
-	ret
+	addi a0, x0, 0
+	ld ra, 72(sp)
+	ld s0, 64(sp)
+	ld s1, 56(sp)
+	ld s2, 48(sp)
+	ld s3, 40(sp)
+	ld s4, 32(sp)
+	ld s5, 24(sp)
+	ld s6, 16(sp)
+	ld s7, 8(sp)
+	ld s8, 0(sp)
+	addi sp, sp, 80
+	jalr x0, ra, 0
+	
