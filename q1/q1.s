20882ed7919e51a9faa2c8db5d6b6545802d779b
Sahishnu Pawan Kumar <sahishnu.pawankumar@research.iiit.ac.in>
2026-04-12 19:21:18 +0530

    updating code slightly

--- q1/q1.s
+++ q1/q1.s
@@ -1,130 +1,91 @@
 .text
+.globl make_node
+.globl insert
+.globl get
+.globl getAtMost
 
-	.globl _make_node
-	.globl _insert
-	.globl _get
-	.globl _getAtMost
-
-	.set NODE_VAL, 0
-	.set NODE_LEFT, 8
-	.set NODE_RIGHT, 16
-	.set NODE_SIZE, 24
-
-_make_node:
-	pushq %rbp
-	movq %rsp, %rbp
-	subq $16, %rsp
-
-	movl %edi, -4(%rbp)
-	movl $NODE_SIZE, %edi
-	call _malloc
-
-	testq %rax, %rax
-	jz L_make_node_done
-
-	movl -4(%rbp), %ecx
-	movl %ecx, NODE_VAL(%rax)
-	movq $0, NODE_LEFT(%rax)
-	movq $0, NODE_RIGHT(%rax)
-
+make_node:
+	addi sp, sp, -16
+	sd ra, 8(sp)
+	sd s0, 0(sp)
+	addi s0, a0, 0
+	addi a0, x0, 24
+	jal ra, malloc
+	beq a0, x0, L_make_node_done
+	sw s0, 0(a0)
+	sd x0, 8(a0)
+	sd x0, 16(a0)
 L_make_node_done:
-	leave
-	ret
+	ld ra, 8(sp)
+	ld s0, 0(sp)
+	addi sp, sp, 16
+	jalr x0, ra, 0
 
-_insert:
-	pushq %rbp
-	movq %rsp, %rbp
-	subq $16, %rsp
-
-	testq %rdi, %rdi
-	jnz L_insert_not_null
-
-	movl %esi, %edi
-	call _make_node
-	jmp L_insert_done
-
+insert:
+	addi sp, sp, -32
+	sd ra, 24(sp)
+	sd s0, 16(sp)
+	sd s1, 8(sp)
+	bne a0, x0, L_insert_not_null
+	addi a0, a1, 0
+	jal ra, make_node
+	jal x0, L_insert_done
 L_insert_not_null:
-	movq %rdi, -8(%rbp)
-	movl %esi, -12(%rbp)
-
-	movl NODE_VAL(%rdi), %edx
-	cmpl %edx, %esi
-	je L_insert_done_root
-	jl L_insert_left
-
-	movq NODE_RIGHT(%rdi), %rdi
-	movl -12(%rbp), %esi
-	call _insert
-	movq -8(%rbp), %rcx
-	movq %rax, NODE_RIGHT(%rcx)
-	jmp L_insert_done_root
-
+	addi s0, a0, 0
+	addi s1, a1, 0
+	lw t0, 0(s0)
+	beq t0, s1, L_insert_done_root
+	blt s1, t0, L_insert_left
+	ld a0, 16(s0)
+	addi a1, s1, 0
+	jal ra, insert
+	sd a0, 16(s0)
+	jal x0, L_insert_done_root
 L_insert_left:
-	movq NODE_LEFT(%rdi), %rdi
-	movl -12(%rbp), %esi
-	call _insert
-	movq -8(%rbp), %rcx
-	movq %rax, NODE_LEFT(%rcx)
-
+	ld a0, 8(s0)
+	addi a1, s1, 0
+	jal ra, insert
+	sd a0, 8(s0)
 L_insert_done_root:
-	movq -8(%rbp), %rax
-
+	addi a0, s0, 0
 L_insert_done:
-	leave
-	ret
+	ld ra, 24(sp)
+	ld s0, 16(sp)
+	ld s1, 8(sp)
+	addi sp, sp, 32
+	jalr x0, ra, 0
 
-_get:
-	pushq %rbp
-	movq %rsp, %rbp
-	movq %rdi, %rax
-
+get:
 L_get_loop:
-	testq %rax, %rax
-	jz L_get_done
-
-	movl NODE_VAL(%rax), %edx
-	cmpl %edx, %esi
-	je L_get_done
-	jl L_get_left
-
-	movq NODE_RIGHT(%rax), %rax
-	jmp L_get_loop
-
+	beq a0, x0, L_get_done
+	lw t0, 0(a0)
+	beq t0, a1, L_get_done
+	blt a1, t0, L_get_left
+	ld a0, 16(a0)
+	jal x0, L_get_loop
 L_get_left:
-	movq NODE_LEFT(%rax), %rax
-	jmp L_get_loop
-
+	ld a0, 8(a0)
+	jal x0, L_get_loop
 L_get_done:
-	leave
-	ret
+	jalr x0, ra, 0
 
-_getAtMost:
-	pushq %rbp
-	movq %rsp, %rbp
-
-	movl $-1, %eax
-	movq %rsi, %rcx
-
+getAtMost:
+	addi t1, x0, -1
+	addi t2, a1, 0
 L_getAtMost_loop:
-	testq %rcx, %rcx
-	jz L_getAtMost_done
-
-	movl NODE_VAL(%rcx), %edx
-	cmpl %edi, %edx
-	je L_getAtMost_exact
-	jl L_getAtMost_less
-
-	movq NODE_LEFT(%rcx), %rcx
-	jmp L_getAtMost_loop
-
+	beq t2, x0, L_getAtMost_done
+	lw t0, 0(t2)
+	beq t0, a0, L_getAtMost_exact
+	blt t0, a0, L_getAtMost_less
+	ld t2, 8(t2)
+	jal x0, L_getAtMost_loop
 L_getAtMost_less:
-	movl %edx, %eax
-	movq NODE_RIGHT(%rcx), %rcx
-	jmp L_getAtMost_loop
-
+	addi t1, t0, 0
+	ld t2, 16(t2)
+	jal x0, L_getAtMost_loop
 L_getAtMost_exact:
-	movl %edx, %eax
-
+	addi t1, t0, 0
 L_getAtMost_done:
-	leave
-	ret
+	addi a0, t1, 0
+	jalr x0, ra, 0
+	
